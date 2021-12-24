67ef3d36e1f0ab7afa8d26ccfdd2f45903b8af79
justinwu <abc36334720@gmail.com>
2021-12-24 16:27:12 +0800

    本地端application錯誤修正

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.7.4'
+ruby '2.7.5'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.6'
@@ -18,6 +18,8 @@ gem 'uglifier', '>= 1.3.0'
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
+#For Windows it works with CofferScript v1.8
+gem 'coffee-script-source', '1.8.0'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema.define(version: 2021_07_12_165026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
   create_table "line_items", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "order_id"
@@ -36,6 +35,7 @@ ActiveRecord::Schema.define(version: 2021_07_12_165026) do
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
+  
   create_table "todos", force: :cascade do |t|
     t.string "name"
     t.string "desc"
@@ -58,7 +58,4 @@ ActiveRecord::Schema.define(version: 2021_07_12_165026) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "line_items", "orders"
-  add_foreign_key "line_items", "users"
-  add_foreign_key "orders", "users"
 end
